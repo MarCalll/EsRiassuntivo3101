df0bf92180c03bb38dc40d3ed853d0c1fc2e26ab
Marco <126587897+MarCalll@users.noreply.github.com>
2025-01-31 16:12:59 +0100

    Add files via upload

--- EsRiassuntivoP1.py
+++ EsRiassuntivoP1.py
@@ -5,13 +5,29 @@
 def creaAccount():
     nomeUtente=input("Inserire nuovo nome utente: ")
     password=input("Inserire password: ")
-    return [nomeUtente,password]
+    return [nomeUtente,password,[],[],[]]
 
 account=creaAccount()
 
 def aggiungiNumero():
     numero=input("Inserire nuovo numero di telefono: ")
-    account.append(numero)
+    account[2].append([numero])
     print(account)
     
 aggiungiNumero()
+
+def checkNomeUtente(nuovoNomeUtente):
+    if account[0]==nuovoNomeUtente:
+        return True
+    else:
+        
+
+def modificaAccount():
+    print("Cosa vuoi modificare?")
+    print("1.Nome utente")
+    print("2.Password")
+    print("3.Numero di telefono")
+    scelta=int(input())
+    if scelta==1:
+        nuovoNomeUtente=input("Inserisci nuovo nome utente ")
+        checkNomeUtente(nuovoNomeUtente)
